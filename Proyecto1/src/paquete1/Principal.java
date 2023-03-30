fbddf3c9ba4d4828f4a8be727cea89ec1d387dd5
Blanca <Blanca@BLANCA.mshome.net>
2023-03-30 09:59:27 +0200

    Segundo cambio

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 	System.out.println("Estamos haciendo control de versiones con Git");
-
+	
+	System.out.println("Escribimos algo para que haya un cambio y hacer otro commit");
 	}
 
 }
